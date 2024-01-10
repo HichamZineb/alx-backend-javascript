96e7e6833cd22680bdaada83de049aa468a3e986
HichamZineb <hichamelamine@hotmail.com>
2024-01-10 15:43:15 +0000

    update main.ts/task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,24 +1,64 @@
-
-// Given interface Teacher
-interface Teacher {
+// Exercise 0: Interface for Student
+interface Student {
   firstName: string;
   lastName: string;
+  age: number;
   location: string;
-  fullTimeEmployee: boolean;
 }
 
-// Write an interface named Directors that extends Teacher
+// Exercise 1: Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+// Exercise 2: Extending Teacher interface to create Directors interface
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Example usage
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+// Function definition for printTeacher
+function printTeacher(firstName: string, lastName: string): string {
+  const firstLetter = firstName.charAt(0);
+  const formattedName = `${firstLetter}. ${lastName}`;
+  return formattedName;
+}
 
-console.log(director1);
+// Exercise 3: Interface for the printTeacher function
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Exercise 4: Interface for StudentClass constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+// Interface for the StudentClass methods
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// StudentClass implementation
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
