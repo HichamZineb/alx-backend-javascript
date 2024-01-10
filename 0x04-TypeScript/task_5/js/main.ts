c787434e1d37b27bb631b24522ca3f715b62d0d4
HichamZineb <hichamelamine@hotmail.com>
2024-01-10 16:55:13 +0000

    update task_5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -19,16 +19,3 @@ function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorC
   const totalCredits = subject1.credits + subject2.credits;
   return { credits: totalCredits, __brand: 'MinorCredits' };
 }
-
-// Example usage:
-const majorSubject1: MajorCredits = { credits: 3, __brand: 'MajorCredits' };
-const majorSubject2: MajorCredits = { credits: 4, __brand: 'MajorCredits' };
-
-const minorSubject1: MinorCredits = { credits: 2, __brand: 'MinorCredits' };
-const minorSubject2: MinorCredits = { credits: 1, __brand: 'MinorCredits' };
-
-const totalMajorCredits = sumMajorCredits(majorSubject1, majorSubject2);
-const totalMinorCredits = sumMinorCredits(minorSubject1, minorSubject2);
-
-console.log('Total Major Credits:', totalMajorCredits);
-console.log('Total Minor Credits:', totalMinorCredits);
